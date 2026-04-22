ed316cd7119727019e5a0bc8bbaf9a0b9305c0f9
BHCGH92 <ben.holt1992@live.co.uk>
2026-04-22 21:17:24 +0100

    Register models with admin, add requirements.txt

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Profile, WorkExperience, Education, Skill, Project
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'job_title', 'email', 'phone', 'location')
+    search_fields = ('full_name', 'job_title', 'email', 'phone', 'location')
+
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ('job_title', 'company', 'location', 'start_date', 'end_date', 'is_current')
+    search_fields = ('job_title', 'company', 'location')
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('degree', 'institution', 'field_of_study', 'start_date', 'end_date', 'is_current')
+    search_fields = ('degree', 'institution', 'field_of_study')
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category')
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tech_stack')
+    search_fields = ('title', 'tech_stack')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(WorkExperience, WorkExperienceAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Project, ProjectAdmin)
